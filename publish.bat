ae21ac8cabe720d30a620d8760af531138114fe4
t@123654 <1327444968@qq.com>
2025-05-28 09:46:10 +0800

    1.3.2

--- publish.bat
+++ publish.bat
@@ -4,8 +4,8 @@ REM 读取Python配置文件中的版本号
 for /f "tokens=2 delims==" %%v in ('python -c "from core.config import VERSION; print('VERSION=', VERSION)"') do set VERSION=%%v
 
 echo 当前版本: %VERSION%
-git tag  v%VERSION%
 git add .
+git tag  v%VERSION%
 git commit -m %VERSION%
 git push -u origin main 
 git push origin --tags
