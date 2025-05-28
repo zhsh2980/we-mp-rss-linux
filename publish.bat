a7dadeee5ee3e93ca433bdbd51bab9282390cd2d
t@123654 <1327444968@qq.com>
2025-05-28 09:35:14 +0800

    1.3.2

--- publish.bat
+++ publish.bat
@@ -1,5 +1,5 @@
 @chcp 65001
-REM 读取Python配置文件中的版本号
+@REM 读取Python配置文件中的版本号
 for /f "tokens=2 delims==" %%v in ('python -c "from core.config import VERSION; print('VERSION=', VERSION)"') do set VERSION=%%v
 
 echo 当前版本: %VERSION%
